db03ab0fcb19b41f5cb1803826a81717c15df6e7
Oluwaleke Fakorede <hashilekky@gmail.com>
2018-07-28 11:14:36 +0100

    corrected push mistake

--- configs/dev.env.config.js
+++
@@ -1,8 +0,0 @@
-let express = require('express');
-let app = express();
-if(app.get('env') === 'development') {
-    module.exports = true;
-}
-else{
-    module.exports = false;
-}
--- models/saving.js
+++
@@ -1,2 +0,0 @@
-const mongoose = require('mongoose');
-let Schema = mongoose.Schema;
